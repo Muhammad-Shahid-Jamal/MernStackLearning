a181c14e00024cf80a7743d5fa013f0d32e87711
Zeeshan Azin <Zeeshan.Azin@omangrid.com>
2022-03-07 23:37:54 +0400

    commit by zeeshan

--- employee.js
+++ employee.js
@@ -1,12 +1,18 @@
-const {db} = require('./data')
+const { db } = require('./data')
 
 const fetchUserById = (id, callback) => {
-    //setTimeout(() => {
+    if (typeof id === 'number') {
+
+        setTimeout(() => {
         const results = db.users.find(i => i.id === id);
         results ?
             callback(null, results) :
             callback('Not found', null);
-    //}, 2000);
+        }, 2000);
+    }
+    else {
+        callback('The number provided must be of type number', null);
+    }
 };
 
 fetchUserById(1, (err, user) => {
@@ -16,3 +22,5 @@ fetchUserById(1, (err, user) => {
     }
     console.log('User Details:', user);
 });
+
+console.log('Execution after fetchUserById');
---
+++ selfexecutingFunc.js
@@ -0,0 +1,12 @@
+/* Self Executing Anonymous Function */
+(function (){
+    console.log('I am calling inside self executing anonymous function');
+})();
+
+
+//==================================
+// arrow function syntax
+//==================================
+(()=>{
+    console.log('arrow self executing anonymous function');
+})();
