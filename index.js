efd250310728cf862dffca1a72a6800fe5c30a35
Zeeshan Azin <Zeeshan.Azin@omangrid.com>
2022-03-11 21:57:48 +0400

    learning/class_6

--- index.js
+++ index.js
@@ -9,31 +9,64 @@ app.use(express.json());
  * const app = require("express")();
  */
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.send('<h2>This is index page</h2>')
 });
 
-app.post("/user",(req,res)=>{
-  // get user data from request and save to file
-  const user = req.body;
-  fs.writeFileSync(`${path_dir}/db/user.txt`,JSON.stringify(user));
-  res.json({
-    success:true
-  });
+// Insert
+app.post("/user", (req, res) => {
+    // get user data from request and save to file
+    const user = req.body;
+    fs.writeFileSync(`${path_dir}/db/user.txt`, JSON.stringify(user));
+    res.json({
+        success: true
+    });
 });
 
-app.get("/user/:id",(req,res)=>{
-  // get user by id from text file
-  const user = fs.readFileSync(`${path_dir}/db/user.txt`);
-  const userJson = JSON.parse(user);
-  res.json({
-    user:{
-      ...userJson,
-      id:req.params.id
-    }
-  })
+
+//Get File
+app.get("/user/:id", (req, res) => {
+    // get user by id from text file
+    const user = fs.readFileSync(`${path_dir}/db/user.txt`);
+    const userJson = JSON.parse(user);
+    res.json({
+        user: {
+            ...userJson,
+            id: req.params.id
+        }
+    })
 });
 
-app.listen(3000,()=>{
+//update file
+app.put("/", (req, res) => {
+    //check if response is coming from postman
+    //console.log(req.body)
+   
+    const user = req.body;
+    fs.appendFileSync(`${path_dir}/db/user.txt`, JSON.stringify(user), (err) =>{
+        if(err){
+            res.json({ result: "Operation Failed" });
+            return;
+        }
+    });  
+    res.json({
+        success: true
+    });     
+});
+
+//Delete file
+app.delete("/", (req, res) => {
+    //check if response is coming from postman
+    //console.log(req.body)
+   
+    const user = req.body;
+    fs.unlinkSync(`${path_dir}/db/user.txt`)
+    res.json({
+        success: true
+    });     
+});
+
+
+app.listen(3000, () => {
     console.log('application started')
 })
