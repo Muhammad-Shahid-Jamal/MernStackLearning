c226ca8840ee88306cf512ffd4f7dda49625acdd
Shahid <shahid.jamal0343@gmail.com>
2022-03-17 01:14:19 +0500

    feat: jsonwebtoken learning and practice

--- Db/connection.js
+++ Db/connection.js
@@ -4,8 +4,12 @@ dotenv.config();
 //MONGO_CONNECTION_URL env MONGO_CONNECTION_URL=your connection url
 const {MONGO_CONNECTION_URL} = process.env;
 async function connectToMongoDb(){
-    await mongoose.connect(MONGO_CONNECTION_URL);
-    console.log("connected to Mongodb");
+    try {
+        await mongoose.connect(MONGO_CONNECTION_URL);
+        console.log("connected to Mongodb");
+    } catch (error) {
+        console.log(error.message);
+    }
 }
 
 module.exports = {
--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const app = express();
 const {connectToMongoDb} = require("./Db/connection");
+const userRouterAuth = require("./routes/auth");
 
 // database connection
 connectToMongoDb();
 
 app.use(express.json());
+//user managment
+app.use("/v1/api/user",userRouterAuth);
 
 app.get("*",(req,res)=>{
   res.status(404).send(`<h1>Page Not Found!</h1>`)
